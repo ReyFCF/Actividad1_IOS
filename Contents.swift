5653b445f860c8f0b242c06ac56ea6579c44b728
MacBook Air <macbookair@Rey-Fernando.local>
2019-01-31 18:06:50 -0600

    first commit

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+var v1 = "esto es swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola mundo, Soy Rey " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
